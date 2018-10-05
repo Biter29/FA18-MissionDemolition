ae3d7e3dc419d618cafff516fb9fa353afbf9e7a
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-10-05 15:52:15 -0400

    All up to first castle
    First castle also had to due edits different from book due to cloud error with random.

---
+++ Assets/CloudCrafter.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloudCrafter : MonoBehaviour {
+    [Header("Set in Inspector")]
+    public int numClouds = 40;
+    public GameObject cloudPrefab;
+    public Vector3 cloudPosMin = new Vector3(-50, -5, 10);
+    public Vector3 cloudPosMax = new Vector3(150, 100, 10);
+    public float cloudScaleMin = 1;
+    public float cloudScaleMax = 3;
+    public float CloudSpeedMult = 0.5f;
+
+    private GameObject[] cloudInstances;
+
+    void Awake()
+    {
+        cloudInstances = new GameObject[numClouds];
+
+        GameObject anchor = GameObject.Find("CloudAnchor");
+
+        GameObject cloud;
+        for(int i = 0; i < numClouds; i++)
+        {
+            cloud = Instantiate<GameObject>(cloudPrefab);
+
+            Vector3 cPos = Vector3.zero;
+            cPos.x = Random.Range(cloudPosMin.x, cloudPosMax.x);
+            cPos.y = Random.Range(cloudPosMin.y, cloudPosMax.y);
+
+            float scaleU = Random.value;
+            if (scaleU * 90 >= 30)
+            {
+                scaleU = 0.2f;
+            }
+            float scaleVal = Mathf.Lerp(cloudPosMin.y, cPos.y, scaleU);
+
+            cPos.z = 100 - 90 * scaleU;
+            cloud.transform.position = cPos;
+            cloud.transform.localScale = Vector3.one * scaleVal;
+
+            cloud.transform.SetParent(anchor.transform);
+            cloudInstances[i] = cloud;
+        }
+
+    }
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		foreach (GameObject cloud in cloudInstances)
+        {
+            float scaleVal = cloud.transform.localScale.x;
+            Vector3 cPos = cloud.transform.position;
+
+            cPos.x -= scaleVal * Time.deltaTime * CloudSpeedMult;
+
+            if (cPos.x <= cloudPosMin.x)
+            {
+                cPos.x = cloudPosMax.x;
+            }
+            cloud.transform.position = cPos;
+        }
+	}
+}
