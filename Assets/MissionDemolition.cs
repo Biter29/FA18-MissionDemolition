b3b6a218ae3ee99a4778ac51947d2d39a55a44b0
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-10-05 18:17:58 -0400

    Final update before completion
    Game will be completed saturday night due to shabbat starting at 6:33, current progress page 639. Only 4 more pages left.

---
+++ Assets/MissionDemolition.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public enum GameMode
+{
+    idle,
+    playing,
+    levelEnd
+}
+public class MissionDemolition : MonoBehaviour {
+    static private MissionDemolition S;
+    [Header("Set in Inspector")]
+    public Text uitLevel;
+    public Text uitShots;
+    public text uitButton;
+    public Vector3 castlePos;
+    public GameObject[] castles;
+
+    [Header("Set Dynamically")]
+    public int level;
+    public int levelMax;
+    public int shotsTaken;
+    public gameObject[] castle;
+    public GameMode mode = GameMode.idle;
+    public string showing = "Show Slingshot";
+	// Use this for initialization
+	void Start () {
+        S = this;
+        level = 0;
+        levelMax = castles.Length;
+        StartLevel();
+	}
+
+    void StartLevel()
+    {
+        if (castle != null)
+        {
+            Destory(castle);
+        }
+
+        GameObject gos = GameObject.FindGameObjectWithTag("Projectile");
+        foreach(GameObject pTemp in gos)
+        {
+            Destroy(pTemp);
+        }
+
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
