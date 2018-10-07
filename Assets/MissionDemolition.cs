6bb56f690701a4dce3b0a974be2f8a1638523eab
Biter29 <31832941+Biter29@users.noreply.github.com>
2018-10-06 21:23:20 -0400

    Game Finished
    Game is finished edits to be made, the book has two flaws which as of now have not been fixed due to fufilling requirement of the game. It deltes the castle object, doing so however makes it so that once game is won an error occurs as the game cannot access previously deleted castles as this is a book error I chose not fix it with the hope points are not deducted. This took around 30 minutes to complete. From last update. Due to shabbos started at 8pm and finished now due to other reasons. As to why it was not sent at 835

--- Assets/MissionDemolition.cs
+++ Assets/MissionDemolition.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 public enum GameMode
@@ -13,7 +12,7 @@ public class MissionDemolition : MonoBehaviour {
     [Header("Set in Inspector")]
     public Text uitLevel;
     public Text uitShots;
-    public text uitButton;
+    public Text uitButton;
     public Vector3 castlePos;
     public GameObject[] castles;
 
@@ -21,7 +20,7 @@ public class MissionDemolition : MonoBehaviour {
     public int level;
     public int levelMax;
     public int shotsTaken;
-    public gameObject[] castle;
+    public GameObject castle;
     public GameMode mode = GameMode.idle;
     public string showing = "Show Slingshot";
 	// Use this for initialization
@@ -36,20 +35,79 @@ public class MissionDemolition : MonoBehaviour {
     {
         if (castle != null)
         {
-            Destory(castle);
+            Destroy(castle);
         }
 
-        GameObject gos = GameObject.FindGameObjectWithTag("Projectile");
+        GameObject[] gos = GameObject.FindGameObjectsWithTag("Projectile");
         foreach(GameObject pTemp in gos)
         {
             Destroy(pTemp);
         }
 
-        
+        castle = Instantiate<GameObject>(castles[level]);
+        castle.transform.position = castlePos;
+        shotsTaken = 0;
+
+        SwitchView("Show Both");
+        ProjectileLine.S.Clear();
+        Goal.goalMet = false;
+        UpdateGUI();
+
+        mode = GameMode.playing;
+    }
+    void UpdateGUI()
+    {
+        uitLevel.text = "Level: " + (level + 1) + " of " + levelMax;
+        uitShots.text = "Shots Taken: " + shotsTaken;
     }
 	
 	// Update is called once per frame
 	void Update () {
-		
+        UpdateGUI();
+        if((mode == GameMode.playing) && Goal.goalMet)
+        {
+            mode = GameMode.levelEnd;
+            SwitchView("Show Both");
+            Invoke("NextLevel", 2f);
+        }
 	}
+
+    void NextLevel()
+    {
+        level++;
+        if (level == levelMax)
+        {
+            level = 0;
+        }
+        StartLevel();
+    }
+    public void SwitchView(string eView = "")
+    {
+        if (eView == "")
+        {
+            eView = uitButton.text;
+        }
+        showing = eView;
+        switch (showing)
+        {
+            case "Show Slingshot":
+                FollowCam.POI = null;
+                uitButton.text = "Show Castle";
+                break;
+            case "Show Castle":
+                FollowCam.POI = null;
+                uitButton.text = "Show Both";
+                break;
+            case "Show Both":
+                FollowCam.POI = GameObject.Find("ViewBoth");
+                uitButton.text = "Show Slingshot";
+                break;
+        }
+    }
+    public static void ShotFired()
+    {
+        S.shotsTaken++;
+    }
+
+
 }
